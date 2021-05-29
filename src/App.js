35787e6d6fdb3ee7e803ead038da2a1d25fb9df4
Simon Burg <semburg.sb@gmail.com>
2021-05-29 12:50:36 +0200

    added array in hook plus more props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'Feb 6th at 1:30px',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Feb 5th at 3:30pm',
+        reminder: true,
+    },
+])
   return (
     <div className="container">
       <Header  />
-
+      <Tasks tasks={tasks} />
     </div>
   );
 }
