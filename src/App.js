7c109ca2b6c5de9d17223086d56f2152b36ee280
Simon Burg <semburg.sb@gmail.com>
2021-05-31 12:12:36 +0200

    function props tossing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
@@ -6,28 +6,45 @@ import Tasks from './components/Tasks'
 const App = () => {
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Doctors appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
+      id: 1,
+      text: 'Doctors appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
     },
     {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'Feb 6th at 1:30px',
-        reminder: true,
+      id: 2,
+      text: 'Meeting at school',
+      day: 'Feb 6th at 1:30px',
+      reminder: true,
     },
     {
-        id: 3,
-        text: 'Food shopping',
-        day: 'Feb 5th at 3:30pm',
-        reminder: true,
+      id: 3,
+      text: 'Food shopping',
+      day: 'Feb 5th at 3:30pm',
+      reminder: false,
     },
-])
+  ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task)=> task.id === id
+    ? { ...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
-      <Header  />
-      <Tasks tasks={tasks} />
+      <Header />
+
+      { tasks.length > 0 ?
+        (<Tasks tasks={tasks} onDelete={deleteTask} onToggle = {toggleReminder} />)
+        : ("No tasks to Show")
+      }
+
     </div>
   );
 }
